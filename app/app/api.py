6a2229ac8e46bbc068f8ae99e0979b23f19a7b1c
Jarno Jellesma <jarnojellesma@gmail.com>
2020-01-20 15:43:58 +0100

    Add racing tracks

--- app/app/api.py
+++ app/app/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 
 
 api_bp = Blueprint('api_bp', __name__)
---
+++ app/app/datamanager.py
@@ -0,0 +1,118 @@
+RACING_TRACKS = [
+    {
+        "title": "MELBOURNE GRAND PRIX CIRCUIT",
+        "city": "Melbourne",
+        "country": "Australia",
+    },
+    {
+        "title": "BAHRAIN INTERNATIONAL CIRCUIT",
+        "city": None,
+        "country": "Bahrain",
+    },
+    {
+        "title": "HANOI CIRCUIT",
+        "city": "Hanoi",
+        "country": "Vietnam",
+    },
+    {
+        "title": "SHANGHAI INTERNATIONAL CIRCUIT",
+        "city": "Shanghai",
+        "country": "China",
+    },
+    {
+        "title": "CIRCUIT ZANDVOORT",
+        "city": "Zandvoort",
+        "country": "Netherlands ",
+    },
+    {
+        "title": "CIRCUIT DE BARCELONA-CATALUNYA",
+        "city": "",
+        "country": "Spain",
+    },
+    {
+        "title": "CIRCUIT DE MONACO",
+        "city": "Monte Carlo",
+        "country": "Monaco",
+    },
+    {
+        "title": "BAKU CITY CIRCUIT",
+        "city": "Baku",
+        "country": "Azerbaijan",
+    },
+    {
+        "title": "CIRCUIT GILLES-VILLENEUVE",
+        "city": "Montreal",
+        "country": "Canada",
+    },
+    {
+        "title": "CIRCUIT PAUL RICARD",
+        "city": "",
+        "country": "France",
+    },
+
+    {
+        "title": "RED BULL RING",
+        "city": "Spielberg",
+        "country": "Austria",
+    },
+    {
+        "title": "SILVERSTONE CIRCUIT",
+        "city": "",
+        "country": "Great Britain",
+    },
+    {
+        "title": "HUNGARORING",
+        "city": "Budapest",
+        "country": "Hungary",
+    },
+    {
+        "title": "CIRCUIT DE SPA-FRANCORCHAMPS",
+        "city": "",
+        "country": "Belgium",
+    },
+    {
+        "title": "AUTODROMO NAZIONALE MONZA",
+        "city": "",
+        "country": "Italy",
+    },
+    {
+        "title": "MARINA BAY STREET CIRCUIT",
+        "city": "",
+        "country": "Singapore",
+    },
+
+    {
+        "title": "SOCHI AUTODROM",
+        "city": "",
+        "country": "Russia",
+    },
+    {
+        "title": "SUZUKA INTERNATIONAL RACING COURSE",
+        "city": "Ino",
+        "country": "Japan",
+    },
+    {
+        "title": "CIRCUIT OF THE AMERICAS",
+        "city": "Austin",
+        "country": "United States",
+    },
+    {
+        "title": "AUTÓDROMO HERMANOS RODRÍGUEZ",
+        "city": "Mexico City",
+        "country": "Mexico",
+    },
+    {
+        "title": "AUTÓDROMO JOSÉ CARLOS PACE",
+        "city": "Sao Paulo",
+        "country": "Brazil",
+    },
+    {
+        "title": "YAS MARINA CIRCUIT",
+        "city": "",
+        "country": "Abu Dhabi",
+    },
+]
+
+
+def get_racing_tracks():
+    return RACING_TRACKS
