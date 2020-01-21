1c10d4707b6d39eb63ed3be131abadb94ab83a75
Jarno Jellesma <jarnojellesma@gmail.com>
2020-01-21 12:02:50 +0100

    Add const package

---
+++ app/app/const.py
@@ -0,0 +1,7 @@
+import os
+
+
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(SRC_DIR)
+REPO_DIR = os.path.dirname(ROOT_DIR)
+DATA_DIR = os.path.join(REPO_DIR, "data")
