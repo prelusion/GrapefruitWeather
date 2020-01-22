d1b4765519c806373a7b8e3df64b5beebdecad1b
Leon <Leonjelsma@gmail.com>
2020-01-22 12:16:17 +0100

    Fixed station dataset and added country table with IDs

--- app/app/fileaccess.py
+++ app/app/fileaccess.py
@@ -5,7 +5,7 @@ import os
 def get_stations_db():
     stations = []
     print(os.getcwd())
-    with open("../../DB/stations.csv") as csvfile:
+    with open("../database/stations.csv") as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             stations.append(row)
