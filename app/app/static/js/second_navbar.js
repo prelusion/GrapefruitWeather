4a8245b7670a7f4339a53f2c575a8add0a3a92f3
Rick Huizing <RickHuizing01@gmail.com>
2020-01-22 09:33:48 +0100

    changed spacing style

--- app/app/static/js/second_navbar.js
+++ app/app/static/js/second_navbar.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $(".dropdown_second_navbar").click(function(){
-        if( $(".menu_second_navbar").css("display") == "none"){
+$(document).ready(function() {
+    $(".dropdown_second_navbar").click(function() {
+        if($(".menu_second_navbar").css("display") == "none") {
             $(".menu_second_navbar").slideDown();
-        }else{
+        } else {
             $(".menu_second_navbar").slideUp();
         }
     });
