56941d0dcffca5de5257e3b7ff6e4d771325b079
Rick Huizing <RickHuizing01@gmail.com>
2020-01-22 09:16:47 +0100

    second navigationbar engine works

--- app/app/static/js/second_navbar.js
+++ app/app/static/js/second_navbar.js
@@ -1,15 +1,9 @@
 $(document).ready(function(){
     $(".dropdown").click(function(){
         if( $(".menu_items").css("display") == "none"){
-
-            $(".menu_items").css("display", "block");
             $(".menu_items").slideDown();
-
         }else{
-            
-            $(".menu_items").css("display", "none");
             $(".menu_items").slideUp();
-
         }
     });
 });
