62d9e9bbb2931ce4dc473d5d278af3f581ad4e88
Rick Huizing <RickHuizing01@gmail.com>
2020-01-22 12:33:53 +0100

    fixed annoying resizing bug with jquery instead of css

--- app/app/static/js/second_navbar.js
+++ app/app/static/js/second_navbar.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+    function check_window_size(){
+        var windowsize = $(window).width();
+        if(windowsize <= 1185){
+            $(".dropdown_second_navbar").css("display", "block");
+        }else {
+            $(".dropdown_second_navbar").css("display", "none");
+        }
+
+        if(windowsize >= 1185 && ($(".menu_second_navbar").css("display") == "block")){
+            $(".menu_second_navbar").css("display", "none");
+        }
+    }
+
+    check_window_size();
+
+    $(window).resize(function(){
+        check_window_size();
+    });
+
     $(".dropdown_second_navbar").click(function() {
         if($(".menu_second_navbar").css("display") == "none") {
             $(".menu_second_navbar").slideDown();
@@ -7,5 +26,7 @@ $(document).ready(function() {
         }
     });
 });
+
+
 
 
