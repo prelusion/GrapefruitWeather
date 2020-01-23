31205ee6380717d29b147bd869344997afb9f2bc
Prelusion <d.g.d.van.der.waal@st.hanze.nl>
2020-01-23 11:29:11 +0100

    Error handling

--- app/app/static/js/filter.js
+++ app/app/static/js/filter.js
@@ -4,24 +4,55 @@ $(document).ready(function() {
         $("#latitude").val(jsonArray[$(this).val()-1].latitude);
         $("#longitude").val(jsonArray[$(this).val()-1].longitude);
         $("#country").val(jsonArray[$(this).val()-1].country);
+        getTrackFilter(true);
+    });
+
+    $("#latitude").on('input', function() {
+        getTrackFilter();
+    });
+    $("#longitude").on('input', function() {
         getTrackFilter();
     });
 
-    $("#limit").change(function() {
+    $("#limit").on('input', function() {
         getTrackFilter();
     });
 
-    $("#range").change(function() {
+    $("#range").on('input', function() {
         getTrackFilter();
     });
 
-    function getTrackFilter() {
+    function getTrackFilter(bool, obj) {
         var trackID = $("#track").val();
         var latitude = $("#latitude").val();
         var longitude = $("#longitude").val();
         var country = $("#country").val();
         var limit = $("#limit").val();
         var range = $("#range").val();
+        var error = false;
+        if (isNaN(latitude)) {
+            $("#latitude_error").show();
+            error = true;
+        }
+        if(isNaN(longitude)) {
+            $("#longitude_error").show();
+            error = true;
+        }
+        if(isNaN(limit)) {
+            $("#limit_error").show();
+            error = true;
+        }
+        if(isNaN(range)) {
+            $("#range_error").show();
+            error = true;
+        }
+        if(error === true) return;
+        $("#latitude_error").hide();
+        $("#longitude_error").hide();
+        $("#limit_error").hide();
+        $("#range_error").hide();
+
+        setMapView(latitude, longitude);
         $.get("http://127.0.0.1:5000/api/tracks?id="+trackID+"&country="+country, function(result) {
             console.log(result);
         });
--- app/app/static/js/map.js
+++ app/app/static/js/map.js
@@ -1,14 +1,16 @@
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+$(document).ready(function() {
+    map = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-greenIcon = new L.Icon({
-    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    greenIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
 });
