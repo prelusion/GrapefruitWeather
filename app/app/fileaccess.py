ec351bcc7d2b839baccc8149a4dd4331b6dce51a
Jarno Jellesma <jarnojellesma@gmail.com>
2020-01-23 16:28:10 +0100

    Convert RACING_TRACKS constant to csv file

--- app/app/fileaccess.py
+++ app/app/fileaccess.py
@@ -1,12 +1,13 @@
 import csv
 import os
-
-from app import const
+from pprint import pprint
+from app import const, util
 
 _stations_data = None
+_tracks_data = None
 
 
-def get_stations_db():
+def get_stations():
     global _stations_data
 
     if not _stations_data:
@@ -25,3 +26,21 @@ def get_stations_db():
                 })
 
     return _stations_data
+
+
+def _convert_track(track):
+    track["id"] = int(track["id"])
+    track["latitude"] = float(track["latitude"])
+    track["longitude"] = float(track["longitude"])
+    return track
+
+
+def get_tracks():
+    global _tracks_data
+
+    if not _tracks_data:
+        with open(os.path.join(const.DATA_DIR, "tracks.csv")) as f:
+            _tracks_data = util.csv_to_array_of_dicts(f)
+            _tracks_data = list(map(_convert_track, _tracks_data))
+
+    return _tracks_data
