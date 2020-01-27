688102be86c4c1da27ff58858ed454ec7132ab86
Rick Huizing <RickHuizing01@gmail.com>
2020-01-27 14:37:43 +0100

    made working graph

--- app/app/static/js/pressure_chart.js
+++ app/app/static/js/pressure_chart.js
@@ -1,67 +1,75 @@
-$(document).ready(function(){
-
-    var timelist = [];
-    var pressurelist = [];
-    var stations = [93590,589210];
-    var timeInterval = 120;
-    var limit = 120;
+var graphAirStations = [];
+var timeInterval = 120;
+var limit = 120;
+var pressure_timelist = [];
+var pressurelist = [];
 
 
-    function plot(timestamps, pressure){
-        var myLineChart = new Chart($("#pressure_chart"), {
-            type: 'line',
-            data: {
-                labels: timestamps,
-                datasets: [{ 
-                    data: pressure,
-                    label: "pressure",
-                    borderColor: "#3e95cd",
-                    fill: true
-                }]
-            },
-            options: {
-                title: {
-                    display: true,
-                    text: "Airpressure"
-                },
-                animation: false
-            }
-        });
-        $("#time_label").text("Time(realtime): " + timelist[timelist.length-1]);
-        $("#pressure_label").text("Airpressure(realtime): " + pressurelist[pressurelist.length-1]);
-        $(".air .footer_container").css("background-color", "white");
-    }
-
-    function setAirStations() {
-        stations = getAirStations();
-    }
-
-    
-    function get_data() {
-        let ur = stations.join();
+function get_pressure_data() {
+    if(graphAirStations.length != 0){
+        let ur = graphAirStations.join();
         $.get("http://127.0.0.1:5000/api/measurements/airpressure?limit=" + limit +"&stations=" + ur, function(result) {
-            if(timelist.length == 0){
+            if(pressure_timelist.length == 0){
                 for(x = timeInterval - 1; x >= 0; x--){
-                    timelist.push(("" + result.data[x][0].substring(17,25)));
+                    pressure_timelist.push(("" + result.data[x][0].substring(17,25)));
                     pressurelist.push(result.data[x][1]);
-                    limit = 1;
                 }
+                limit = 1;
             } else {
-                    console.log(limit);
-                    timelist.shift();
-                    timelist.push("" + result.data[0][0].substring(17,25));
+                    pressure_timelist.shift();
+                    pressure_timelist.push("" + result.data[0][0].substring(17,25));
 
                     pressurelist.shift();
                     pressurelist.push(result.data[0][1]);
             }       
         });  
-    }      
+    }
+}     
 
-    setInterval(function() {
-        get_data();
-        plot(timelist, pressurelist);        
-    }, 1000);
+function setAirStations() {
+    graphAirStations = [];
+    pressure_timelist = [];
+    pressurelist = [];
+    limit = 120;
+    graphAirStations = getAirStations();
+    plot_pressure_graph();
+}
 
-    
-});
+function plot_pressure_graph(){
+    get_pressure_data();
+    draw(pressure_timelist, pressurelist);    
+}
+
+function draw(timestamps, pressure){
+    var myLineChart = new Chart($("#pressure_chart"), {
+        type: 'line',
+        data: {
+            labels: timestamps,
+            datasets: [{ 
+                data: pressure,
+                label: "pressure",
+                borderColor: "#3e95cd",
+                fill: true
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "Airpressure"
+            },
+            animation: false
+        }
+    });
+    $("#time_label").text("Time(realtime): " + pressure_timelist[pressure_timelist.length-1]);
+    $("#pressure_label").text("Airpressure(realtime): " + pressurelist[pressurelist.length-1]);
+    $(".air .footer_container").css("background-color", "white");
+}
+
+setInterval( function(){
+    plot_pressure_graph();
+    draw(pressure_timelist, pressurelist);
+}, 1000); 
+
+
+
 
