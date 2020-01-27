5601203b0fd84050471105128b1348bffdee67fd
Prelusion <d.g.d.van.der.waal@st.hanze.nl>
2020-01-27 11:29:16 +0100

    Notice stations

--- app/app/static/js/filter.js
+++ app/app/static/js/filter.js
@@ -11,13 +11,12 @@ $(document).ready(function() {
 });
 
 function getStationsFilter(stationType = "air", custom = false, custom_latitude, custom_longitude, custom_country_id) {
-    var trackID = $("#track").val();
-    var latitude = (custom === false) ? $("#latitude").val() : custom_latitude;
-    var longitude = (custom === false) ? $("#longitude").val() : custom_longitude;
-    var country = (custom === false) ? $("#country").val() : custom_country_id;
-    var limit = $("#limit").val();
-    var range = $("#range").val();
-    var error = false;
+    let latitude = (custom === false) ? $("#latitude").val() : custom_latitude;
+    let longitude = (custom === false) ? $("#longitude").val() : custom_longitude;
+    let country = (custom === false) ? $("#country").val() : custom_country_id;
+    let limit = $("#limit").val();
+    let range = $("#range").val();
+    let error = false;
     if (isNaN(latitude)) {
         $("#latitude_error").show();
         error = true;
@@ -43,24 +42,24 @@ function getStationsFilter(stationType = "air", custom = false, custom_latitude,
         limit = 50;
     }
      $.get("http://127.0.0.1:5000/api/stations?latitude="+latitude+"&longitude="+longitude+"&limit="+limit+"&range="+range, function(result) {
-        markOrCreateStations(result);
+        setAirStationsFromAPI(result);
         setMapView(latitude, longitude, map.getZoom());
     });
     if(stationType === "temperature") {
         $.get("http://127.0.0.1:5000/api/stations?country="+country, function(result) {
-            setTemperatureStations(result);
+            setTemperatureStationsFromAPI(result);
         });
     }
 }
 
 // $.get("http://127.0.0.1:5000/api/stations?latitude="+latitude+"&longitude="+longitude+"&country="+country+"&limit="+limit+"&range="+range, function(result) {
-//         markOrCreateStations(result);
+//         setAirStationsFromAPI(result);
 //         setMapView(latitude, longitude, map.getZoom());
 // });
 
 // console.log(range);
 // $.get("http://127.0.0.1:5000/api/stations?latitude="+latitude+"&longitude="+longitude+"&limit="+limit+"&range="+range, function(result) {
-//     markOrCreateStations(result);
+//     setAirStationsFromAPI(result);
 //     setMapView(latitude, longitude, map.getZoom());
 // });
 // if(stationType === "temperature") {
