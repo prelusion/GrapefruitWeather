48dff35e86ec4c47c4443657d5842d5cd9662b84
Jarno Jellesma <jarnojellesma@gmail.com>
2020-01-28 10:01:42 +0100

    Fix wsmc

--- app/app/wsmc.py
+++ app/app/wsmc.py
@@ -1,12 +1,19 @@
+"""
+Max chunksize for 8 GB RAM on Dell Inspiron 5570 with other programs running:
+187,5 MB    -> Memory error
+157,5 MB    -> Memory Error
+150 MB      -> Fine
+"""
 import datetime
 import math
 import os
+import re
 from collections import OrderedDict
 
 from app import const
 from app import util
 
-PREFERRED_CHUNK_SIZE = 104857600  # +- 100 MB
+MAX_CHUNKSIZE = 100000000  # 100 MB
 
 # Byte count for each field within a measurement
 PROTOCOL_FORMAT_BC = OrderedDict({
@@ -45,17 +52,98 @@ PROTOCOL_FORMAT_BS = OrderedDict({
 })
 
 MEASUREMENT_BYTE_COUNT = sum(PROTOCOL_FORMAT_BC.values())
-CHUNKS = math.trunc(PREFERRED_CHUNK_SIZE / MEASUREMENT_BYTE_COUNT)
-ACTUAL_CHUNK_SIZE = CHUNKS * MEASUREMENT_BYTE_COUNT
 
 
-def read_test_file():
-    filepath = os.path.join(const.MEASUREMENTS_DIR, "pizza.wsmc")
-    return read_file(filepath, ACTUAL_CHUNK_SIZE)
+def determine_chunksize(prefsize=500000):
+    chunks = math.trunc(prefsize / MEASUREMENT_BYTE_COUNT)
+    return chunks * MEASUREMENT_BYTE_COUNT
 
 
-def read_file(filepath, bytecount=None):
+def get_wsmc_files():
+    datadir = const.MEASUREMENTS_DIR
+    files = os.listdir(datadir)
+    files = list(filter(lambda file: file.endswith(".wsmc"), files))
+    files.sort(key=lambda name: int(re.sub('\D', '', name)))
+    return list(((name, os.path.join(datadir, name)) for name in files if ".wsmc" in name))
+
+
+def load_data_per_file(offset):
+    """ Loads wsmc data from the file system.
+    Data is loaded backwards, which implies that the newest data is loaded first.
+    Data is loaded per file, so data from multiple files can not be loaded into memory
+    simultaneously. This means that the file size should be optimized to the maximum chunk size.
+    Based on most recent benchmarks, loading data per file performs better
+    than loading data per chunk.
+    :param offset: amount of chunks skipped when loading into memory
+    :return: data in bytes
+    """
+    files = get_wsmc_files()
+
+    index = (len(files) - 1) - offset
+    if index < 0:
+        return []
+
+    filename, filepath = files[index]
+
+    size = os.path.getsize(filepath)
+    if size > MAX_CHUNKSIZE:
+        raise RuntimeError(
+            f"Unable to load file with a size greater than max chunk size: {MAX_CHUNKSIZE}")
+
+    return read_file(filepath)
+
+
+def load_data_per_chunk(chunksize, offset):
+    """ DEPRECATED: Please use load_data_per_file instead.
+    Based on most recent benchmarks, loading data per file performs better
+    than loading data per chunk.
+    Loads wsmc data from the file system.
+    Data is loaded backwards, which implies that the newest data is loaded first.
+    :param offset: amount of chunks skipped when loading into memory
+    :param chunksize: preferred amount of bytes loaded into memory
+    :return: data in bytes
+    """
+    return _load_data_per_chunk(determine_chunksize(chunksize), offset)
+
+
+def _load_data_per_chunk(chunksize, offset):
+    skipbytes = offset * chunksize
+    spaceleft = chunksize
+    files = get_wsmc_files()
+    totaldata = []
+
+    index = len(files) - 1
+    if index < 0:
+        return []
+
+    while index >= 0 and spaceleft > MEASUREMENT_BYTE_COUNT:
+        filename, filepath = files[index]
+        size = os.path.getsize(filepath)
+
+        if skipbytes > size:
+            skipbytes -= size
+        else:
+            data = read_file(filepath, bytecount=spaceleft, skipbytes=skipbytes)
+            skipbytes = 0
+            spaceleft -= len(data)
+            totaldata.extend(data)
+
+        index -= 1
+
+    return totaldata
+
+
+def read_file(filepath, bytecount=None, skipbytes=None):
+    """ Reads a .wsmc file into memory.
+    This function also checks that the file does not include corrupt measurements.
+    :param filepath: path to .wsmc file
+    :param bytecount: amount of bytes to load into memory, by default loads whole file
+    :param skipbytes: amount of bytes skipped from beginning of file
+    :return: data in bytes
+    """
     with open(filepath, "rb") as f:
+        if skipbytes:
+            f.seek(skipbytes, 0)
         if bytecount:
             data = f.read(bytecount)
         else:
