7957a2a3b3f09e692f4521db9823dac4c329184f
Rick Huizing <rickhuizing01@gmail.com>
2020-01-28 11:10:49 +0100

    changed format and fixed critical section like error

--- app/app/static/js/pressure_chart.js
+++ app/app/static/js/pressure_chart.js
@@ -4,44 +4,9 @@ var limit = 120;
 var pressure_timelist = [];
 var pressurelist = [];
 const refreshrate = 1000;
+var ready = false;
 
-
-function get_pressure_data() {
-    if(graphAirStations.length != 0){
-        let ur = graphAirStations.join();
-        $.get("http://127.0.0.1:5000/api/measurements/airpressure?limit=" + limit +"&stations=" + ur, function(result) {
-            if(pressure_timelist.length == 0){
-                for(x = timeInterval - 1; x >= 0; x--){
-                    pressure_timelist.push(("" + result.data[x][0].substring(17,25)));
-                    pressurelist.push(result.data[x][1]);
-                }
-                limit = 1;
-            } else {
-                    pressure_timelist.shift();
-                    pressure_timelist.push("" + result.data[0][0].substring(17,25));
-
-                    pressurelist.shift();
-                    pressurelist.push(result.data[0][1]);
-            }       
-        });  
-    }
-}     
-
-function setAirStations() {
-    graphAirStations = [];
-    pressure_timelist = [];
-    pressurelist = [];
-    limit = 120;
-    graphAirStations = getAirStations();
-    plot_pressure_graph();
-}
-
-function plot_pressure_graph(){
-    get_pressure_data();
-    draw(pressure_timelist, pressurelist);    
-}
-
-function draw(){
+function draw() {
     if(pressurelist.length == timeInterval && pressure_timelist.length == timeInterval){
         $("#loading_label").hide();
         var myLineChart = new Chart($("#pressure_chart"), {
@@ -68,8 +33,48 @@ function draw(){
     }
 }
 
-setInterval( function(){
+function get_pressure_data() {
+    if(graphAirStations.length != 0) {
+        let ur = graphAirStations.join();
+        $.get("http://127.0.0.1:5000/api/measurements/airpressure?limit=" + limit +"&stations=" + ur, function(result) {
+            if(pressure_timelist.length == 0){
+                for(x = timeInterval - 1; x >= 0; x--){
+                    pressure_timelist.push(("" + result.data[x][0].substring(17,25)));
+                    pressurelist.push(result.data[x][1]);
+                }
+                limit = 1;
+            } else {
+                    pressure_timelist.shift();
+                    pressure_timelist.push("" + result.data[0][0].substring(17,25));
+
+                    pressurelist.shift();
+                    pressurelist.push(result.data[0][1]);
+            }       
+        });  
+    }
+}     
+
+function plot_pressure_graph() {
+    get_pressure_data();
+    draw();    
+}
+
+
+//Reset all datalists and load new 120 records for new history records
+function setAirStations() {
+    graphAirStations = [];
+    pressure_timelist = [];
+    pressurelist = [];
+    limit = 120;
+    graphAirStations = getAirStations();
     plot_pressure_graph();
+    ready = true;
+}
+
+setInterval( function() {
+    if(ready) {
+        plot_pressure_graph();
+    }
 }, refreshrate); 
 
 
