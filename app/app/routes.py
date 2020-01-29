51ec810863d9f6a1b7224fd7156c3b2a52ced76b
Leon <Leonjelsma@gmail.com>
2020-01-29 16:41:14 +0100

    Implemented argon2 encryption

--- app/app/routes.py
+++ app/app/routes.py
@@ -1,9 +1,12 @@
 from flask import current_app as app, request, url_for, redirect, make_response, session
 from flask_login import login_user, login_required
+from passlib.hash import argon2
 
 from app import db, get_user
 from flask import render_template, send_from_directory
 import os
+
+from app.util import encrypt
 
 
 @app.route("/")
@@ -18,10 +21,12 @@ def login():
     error = None
     if request.method == 'POST':
         user = get_user(username=request.form["username"])
-        if user is not False and user.password == request.form["password"]:
+        if user is not False and argon2.verify(request.form["username"], user.password):
             login_user(user)
+
         else:
             error = 'Invalid Credentials. Please try again.'
+
     return render_template('login.html', error=error)
 
 
