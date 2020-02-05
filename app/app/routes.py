893b125a8824092c9fad8e773317aba6ca0bb6ab
Prelusion <d.g.d.van.der.waal@st.hanze.nl>
2020-02-05 10:14:20 +0100

    Reworked CSS and made datalist

--- app/app/routes.py
+++ app/app/routes.py
@@ -10,12 +10,15 @@ from app.util import encrypt
 @app.route("/", methods=['GET', 'POST'])
 @login_required
 def index():
+    ranges = []
+    for value in range (10000):
+        ranges.append(value)
     if request.method == 'POST':
         if "logout" in request.form: 
             logout_user()
             return redirect("/login")
     racing_tracks = db.get_racing_tracks()[1];
-    return render_template('index.html', racing_tracks=racing_tracks)
+    return render_template('index.html', racing_tracks=racing_tracks, ranges_array=ranges)
 
 
 @app.route('/login', methods=['GET', 'POST'])
